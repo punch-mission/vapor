03051d3c9e113a20905e6224970afe684e649bbc
s0larish <risht42@gmail.com>
2025-12-04 12:37:04 -0700

    added function in polarization.py

--- polarization.py
+++ polarization.py
@@ -1,1 +1,26 @@
 import solpolpy as spp
+
+def resolve_bpb(file_list):
+    """
+    Resolve B and pB from a list of input files.
+
+    Parameters
+    ----------
+    file_list : list of str
+        List of file paths (e.g., stereo_0.fts, stereo_120.fts, stereo_240.fts)
+
+    Returns
+    -------
+    tB : numpy.ndarray
+        Total brightness (B)
+    pB : numpy.ndarray
+        Polarized brightness (pB)
+    outbpb : NDCollection
+        Full output from spp.resolve (returned for flexibility)
+    """
+    outbpb = spp.resolve(file_list, 'BpB')
+
+    tB = outbpb['B'].data
+    pB = outbpb['pB'].data
+
+    return tB, pB, outbpb
